9cfa63d252f13fa9f0b41e66f87e4fa8b7a589e7
JIYOUN <jyun6902@gmail.com>
2018-02-28 11:04:26 +0900

    Digit sum

---
+++ q3750.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+using namespace std;
+//cout대신 printf 시간초과해결
+int sum = 0;
+
+int main()
+{
+	int T = 0;
+	scanf("%d", &T);
+
+	for (int t_c = 1; t_c <= T; t_c++)
+	{
+		char s[18];
+		scanf("%s", &s);
+		sum = 0;
+
+		while (1)
+		{
+			sum = 0;
+			for (int t = 0; t < strlen(s); t++)
+				sum += (((int)s[t]) - '0');
+
+			sprintf(s, "%d", sum);
+			if (sum < 10)
+				break;
+		}
+		printf("#%d %d\n", t_c, sum);
+	}
+
+	return 0;
+}
